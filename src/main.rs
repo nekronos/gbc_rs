932f5bfbc3f14777b55529ad85205b228541aba0
nekronos <lende.vegard@gmail.com>
2016-08-30 23:44:46 +0200

    Load rom

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::env;
+use std::path::PathBuf;
+use std::boxed::Box;
+use std::fs::File;
+use std::io::Read;
+
+fn load_bin(path: &PathBuf) -> Box<[u8]> {
+	let mut bytes = Vec::new();
+	let mut file = File::open(path).unwrap();
+	file.read_to_end(&mut bytes).unwrap();
+	bytes.into_boxed_slice()
+}
+
 fn main() {
-    println!("Hello, world!");
+	
+	let rom_path = PathBuf::from(env::args().nth(1).unwrap());
+	let rom_binary = load_bin(&rom_path);
+
+	println!("ROM file name: {:?}", rom_path.file_name().unwrap());
+	println!("ROM size: {:?}", rom_binary.len());
+
+
 }
