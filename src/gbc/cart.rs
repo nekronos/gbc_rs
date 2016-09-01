9ea3a67f84f73ec19410b0cea5f4b4a597df6ce5
nekronos <lende.vegard@gmail.com>
2016-09-01 21:59:15 +0200

    Implement misc functions for cart

--- src/gbc/cart.rs
+++ src/gbc/cart.rs
@@ -1,6 +1,52 @@
+use std::string::String;
 use std::boxed::Box;
 
 #[derive(Debug)]
 pub struct Cart {
-    pub bytes: Box<[u8]>
+    pub bytes: Box<[u8]>,
+}
+
+#[derive(Debug)]
+pub enum CartType {
+    RomMbc5RamBatt,
+    Unsupported,
+}
+
+impl Cart {
+    pub fn name(&self) -> String {
+        let mut name = Vec::new();
+        let mut offset = 0x0134;
+
+        while offset <= 0x0142 {
+            let byte = self.bytes[offset];
+
+            if byte == 0x00 {
+                break;
+            }
+
+            name.push(byte);
+            offset = offset + 1;
+        }
+
+        String::from_utf8(name).unwrap()
+    }
+
+    pub fn cart_type(&self) -> CartType {
+        match self.bytes[0x0147] {
+            0x1b => CartType::RomMbc5RamBatt,
+            _ => CartType::Unsupported,
+        }
+    }
+
+    pub fn rom_size(&self) -> u32 {
+    	match self.bytes[0x0148] {
+    	    0x05 => 1024 * 1024,
+    	    _ => panic!("Unsupported rom size"),
+    	}
+    }
+
+    pub fn bank_count(&self) -> u32 {
+    	self.rom_size() / (1024 * 16)
+    }
+
 }
--- src/main.rs
+++ src/main.rs
@@ -9,20 +9,25 @@ mod gbc;
 use gbc::cart::Cart;
 
 fn load_bin(path: &PathBuf) -> Box<[u8]> {
-	let mut bytes = Vec::new();
-	let mut file = File::open(path).unwrap();
-	file.read_to_end(&mut bytes).unwrap();
-	bytes.into_boxed_slice()
+    let mut bytes = Vec::new();
+    let mut file = File::open(path).unwrap();
+    file.read_to_end(&mut bytes).unwrap();
+    bytes.into_boxed_slice()
 }
 
 fn main() {
-	
-	let rom_path = PathBuf::from(env::args().nth(1).unwrap());
-	let rom_binary = load_bin(&rom_path);
 
-	println!("ROM file name: {:?}", rom_path.file_name().unwrap());
-	println!("ROM size: {:?}", rom_binary.len());
+    let rom_path = PathBuf::from(env::args().nth(1).unwrap());
+    let rom_binary = load_bin(&rom_path);
 
-	let cart = Cart { bytes:rom_binary };
+    println!("ROM file name: {:?}", rom_path.file_name().unwrap());
+    println!("ROM size: {:?}", rom_binary.len());
+
+    let cart = Cart { bytes: rom_binary };
+
+    println!("ROM name: {:?}", cart.name());
+    println!("ROM type: {:?}", cart.cart_type());
+    println!("ROM size: {:?}", cart.rom_size());
+    println!("ROM bank count: {:?}", cart.bank_count());
 
 }
