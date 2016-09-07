d41783838a180c361e7279a9157b010791607395
nekronos <lende.vegard@gmail.com>
2016-09-08 00:06:10 +0200

    Improve disassembler

--- src/gbc/disassembler.rs
+++ src/gbc/disassembler.rs
@@ -2,39 +2,42 @@ use super::cpu::Cpu;
 use super::interconnect::Interconnect;
 use super::opcode::OPCODE_NAME_LUT;
 use super::opcode::CB_OPCODE_NAME_LUT;
+use super::opcode::OPCODE_LENGTHS;
 use std::string::String;
+
+fn disassemble_opcode(opcode: u8, program_counter: u16, interconnect: &Interconnect) -> String {
+    let opcode_length = OPCODE_LENGTHS[opcode as usize];
+    let disasm_str = String::from(OPCODE_NAME_LUT[opcode as usize]);
+    match opcode_length {
+        2 => {
+            let n = interconnect.read(program_counter) as u16;
+            let x = format!("{:02X}", n);
+            disasm_str.replace("n", &x)
+        }
+        3 => {
+            let low = interconnect.read(program_counter) as u16;
+            let high = interconnect.read(program_counter + 1) as u16;
+            let x = format!("{:04X}", (high << 8) | low);
+            disasm_str.replace("nn", &x)
+        }
+        _ => disasm_str,
+    }
+}
+
+fn disassemble_cb_opcode(program_counter: u16, interconnect: &Interconnect) -> String {
+    let opcode = interconnect.read(program_counter);
+    String::from(CB_OPCODE_NAME_LUT[opcode as usize])
+}
 
 pub fn disassemble(program_counter: u16, interconnect: &Interconnect) -> String {
 
-    let mut pc = program_counter;
-    let opcode = interconnect.read(pc);
-    pc = pc + 1;
+    let opcode = interconnect.read(program_counter);
 
-    let mut disasm_str = String::from({
+    let disasm_str = {
         match opcode {
-            0xcb => {
-                let cb_opcode = interconnect.read(pc);
-                pc = pc + 1;
-                CB_OPCODE_NAME_LUT[cb_opcode as usize]
-            }
-            _ => OPCODE_NAME_LUT[opcode as usize],
+            0xcb => disassemble_cb_opcode(program_counter + 1, interconnect),
+            _ => disassemble_opcode(opcode, program_counter + 1, interconnect),
         }
-    });
-
-    if disasm_str.contains("nn") {
-        let low = interconnect.read(pc) as u16;
-        pc = pc + 1;
-        let high = interconnect.read(pc) as u16;
-
-        let address = (high << 8) | low;
-        let address = format!("{:04X}", address);
-
-        disasm_str = disasm_str.replace("nn", &address)
-    } else if disasm_str.contains("n") {
-        let low = interconnect.read(pc);
-        let address = format!("{:02X}", low);
-        disasm_str = disasm_str.replace("n", &address)
-    }
-
+    };
     format!("{:04X}\t\t{}", program_counter, disasm_str)
 }
