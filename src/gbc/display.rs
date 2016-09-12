93159c794af1576053c60b10021a5f74e44d11cf
nekronos <lende.vegard@gmail.com>
2016-09-12 23:31:31 +0200

    Add LCDC masks

--- src/gbc/display.rs
+++ src/gbc/display.rs
@@ -1,3 +1,13 @@
+/*
+const LCD_DISPLAY_ENABLE: u8 = 0b1000_0000;
+const WINDOW_TILE_MAP_DISPLAY_SELECT: u8 = 0b0100_0000;
+const WINDOW_DISPLAY_ENABLE: u8 = 0b0010_0000;
+const BG_WINDOW_TILE_DATE_SELECT: u8 = 0b0001_0000;
+const BG_TILE_MAP_DISPLAY_SELECT: u8 = 0b0000_1000;
+const OBJ_SIZE: u8 = 0b0000_0100;
+const OBJ_DISPLAY_ENABLE: u8 = 0b0000_0010;
+const BG_DISPLAY: u8 = 0b0000_0001;
+*/
 #[derive(Debug)]
 pub struct Display {
     
@@ -5,12 +15,15 @@ pub struct Display {
 
 impl Display {
     pub fn new() -> Display {
-        Display {}
+        Display { lcdc: 0 }
     }
 
     pub fn read(&self, address: u16) -> u8 {
-        0
+        panic!("Display read not implemented");
     }
 
-    pub fn write(&mut self, address: u16, value: u8) {}
+    pub fn write(&mut self, address: u16, value: u8) {
+        panic!("Display write not implemented");
+    }
+
 }
