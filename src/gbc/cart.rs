99183e1fc39598135ea559bbda791cc8a18900c9
nekronos <lende.vegard@gmail.com>
2016-09-19 00:08:13 +0200

    Add TODO for cart.gameboy_type

--- src/gbc/cart.rs
+++ src/gbc/cart.rs
@@ -94,6 +94,7 @@ impl Cart {
 
     pub fn gameboy_type(&self) -> GameboyType {
         match self.bytes[0x0143] {
+            // TODO: confirm that this is correct
             0x80 | 0xc0 => GameboyType::Cgb,
             _ => GameboyType::Gb,
         }
