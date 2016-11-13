dd00c6e35408070052035226e63a59fb4a87f3c6
nekronos <lende.vegard@gmail.com>
2016-11-13 12:42:30 +0100

    Implement proper MBC1

--- src/gbc/mbc/mbc1.rs
+++ src/gbc/mbc/mbc1.rs
@@ -3,20 +3,54 @@ use super::MbcInfo;
 
 #[derive(Debug)]
 pub struct Mbc1 {
-    bank_select: u8,
-    ram: Option<Box<[u8]>>,
+    ram_write_protected: bool,
+    rom_bank_0: u8,
+    rom_bank_1: u8,
+    ram_select: u8,
+    rom_offset: usize,
+    ram_offset: usize,
+    ram: Box<[u8]>,
 }
 
 impl Mbc1 {
     pub fn new(mbc_info: MbcInfo) -> Mbc1 {
         Mbc1 {
-            bank_select: 0x01,
+            ram_write_protected: true,
+            rom_bank_0: 0,
+            rom_bank_1: 0,
+            ram_select: 0,
+            rom_offset: 0,
+            ram_offset: 0,
             ram: if let Some(ram_info) = mbc_info.ram_info {
-                Some(vec![0; ram_info.size as usize].into_boxed_slice())
+                vec![0; ram_info.size as usize].into_boxed_slice()
             } else {
-                None
+                vec![0; 0].into_boxed_slice()
             },
         }
+    }
+
+    fn update_rom_offset(&mut self) {
+        let bank_0 = if self.rom_bank_0 & 0x0f == 0 {
+            self.rom_bank_0 | 0x01
+        } else {
+            self.rom_bank_0
+        } as usize;
+
+        let bank_1 = if self.ram_select == 0 {
+            self.rom_bank_1
+        } else {
+            0
+        } as usize;
+
+        self.rom_offset = bank_0 * 0x4000 + bank_1 * 512 * 1024;
+    }
+
+    fn update_ram_offset(&mut self) {
+        self.ram_offset = if self.ram_select == 1 {
+            self.rom_bank_1 as usize * 8 * 1024
+        } else {
+            0
+        };
     }
 }
 
@@ -24,25 +58,28 @@ impl Mbc for Mbc1 {
     fn read(&self, rom: &Box<[u8]>, addr: u16) -> u8 {
         match addr {
             0x0000...0x3fff => rom[addr as usize],
-            0x4000...0x7fff => {
-                rom[(addr as u32 - 0x4000 + self.bank_select as u32 * 0x4000) as usize]
-            }
-            _ => panic!("Mbc1::read: address out of range 0x{:x}", addr),
+            0x4000...0x7fff => rom[addr as usize - 0x4000 + self.rom_offset],
+            _ => panic!("Address out of range 0x{:x}", addr),
         }
     }
 
     fn write(&mut self, addr: u16, val: u8) {
         match addr {
-            0x2000...0x3fff => self.bank_select = if (val & 0xf) == 0 { val | 0x01 } else { val },
-            _ => panic!("Mbc1::write address out of range 0x{:x}", addr),
+            0x0000...0x1fff => self.ram_write_protected = val == 0x0a,
+            0x2000...0x3fff => self.rom_bank_0 = val,
+            0x4000...0x5fff => self.rom_bank_1 = val,
+            0x6000...0x7fff => self.ram_select = val,
+            _ => panic!("Illegal address: 0x{:x}", addr),
         }
+        self.update_rom_offset();
+        self.update_ram_offset()
     }
 
-    #[allow(unused_variables)]
     fn read_ram(&self, addr: u16) -> u8 {
-        0
+        self.ram[addr as usize - 0xa000 + self.ram_offset]
     }
 
-    #[allow(unused_variables)]
-    fn write_ram(&mut self, addr: u16, val: u8) {}
+    fn write_ram(&mut self, addr: u16, val: u8) {
+        self.ram[addr as usize - 0xa000 + self.ram_offset] = val
+    }
 }
